d9e3ac361c55148fbe39aa8dd51fe7885bd10d43
torarnehave <torarnehave@gmail.com>
2024-09-01 18:01:13 +0200

    refactor: Update og:image meta tag in Dropbox route

--- routes/dropbox.js
+++ routes/dropbox.js
@@ -775,7 +775,7 @@ router.get('/list-image-files', ensureValidToken, async (req, res) => {
             <!DOCTYPE html>
             <html>
             <head>
-            <meta property="og:image" content="${imageTag}" />
+            <meta property="og:image" content="${fullUrl}" />
                 <title>SlowYou™ Blog</title>
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
                 <link rel="stylesheet" href="/markdown.css">
