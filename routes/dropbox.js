f8658c4face8d60472c769d367aef889bc93a3d5
torarnehave <torarnehave@gmail.com>
2024-09-01 22:03:25 +0200

    refactor: Update Dropbox route to include Open Graph meta tags for image files

--- routes/dropbox.js
+++ routes/dropbox.js
@@ -803,8 +803,10 @@ router.get('/list-image-files', ensureValidToken, async (req, res) => {
             <html>
             <head>
                 <meta property="og:image" content="${fullImageUrl}" />
-                <meta name="description" content="${description}" />
-                <title>${title}</title>
+                <meta property="og:description" content="${description}" />
+                <meta property="og:title" content="${title}" />
+
+                <title>MystMkra.io Blog</title>
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
                 <link rel="stylesheet" href="/markdown.css">
             </head>
