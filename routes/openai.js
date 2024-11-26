e70d322e6f87e6bdb79bc64d2aeb4adb1fc7e103
torarnehave <torarnehave@gmail.com>
2024-11-26 12:58:26 +0100

    Increase limit in performSearch function to return top 5 most similar documents

--- routes/openai.js
+++ routes/openai.js
@@ -91,7 +91,7 @@ const performSearch = async (query) => {
                 },
             },
             { $sort: { similarity: -1 } },
-            { $limit: 2 }, // Limit to top 2 most similar documents
+            { $limit: 5 }, // Limit to top 2 most similar documents
             {
                 $project: {
                     _id: 0, // Exclude the _id field
