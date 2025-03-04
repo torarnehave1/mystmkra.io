99ad353c4ec02c72db9ba9a3bc57ab38f9b787b2
torarnehave <torarnehave@gmail.com>
2025-03-05 00:07:30 +0100

    Fix import path case sensitivity for Process model in ViewProcessHeader.js

--- modules/greenbot/ViewProcessHeader.js
+++ modules/greenbot/ViewProcessHeader.js
@@ -16,7 +16,7 @@
  */
 
 import dotenv from 'dotenv';
-import Process from '../greenbot/models/Process.js';
+import Process from '../greenbot/models/process.js';
 
 // Load environment variables from .env file
 dotenv.config();
